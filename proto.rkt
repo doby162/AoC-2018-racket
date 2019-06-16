3145082891e190d7b238d2e4e468214e89918d90
Michael Dorian <doby162@gmail.com>
2019-06-16 13:25:55 -0500

    working bitlist to int

--- proto.rkt
+++ proto.rkt
@@ -14,13 +14,21 @@
   (let ([value (bytes->int (subbytes in pointer (+ pointer length)))])
     (set! pointer (+ pointer length))
     value))
+;turn decimal numbers into a string of bits for twiddling with
+(define (int->bits dec [top true])
+  (let([bin-buffer (list)])
+    (when (> dec 0)    
+      (set! bin-buffer (append bin-buffer (list (modulo dec 2))))
+      (set! bin-buffer (append bin-buffer (int->bits (floor (/ dec 2)) false))))
+    (when top (set! bin-buffer (reverse bin-buffer)))
+    bin-buffer))
 ;define a record
 (struct record (type timestamp user-id dollar-amount))
 (define records (list))
 ;and create a record
 (define (read-record file pointer)
   (set! records (append records (list
-                                 (record (reader 1) (reader 4) (reader 8) (reader 8))))))
+                                 (record (reader 1) (reader 4) (reader 8) (int->bits (reader 8)))))))
 ;display a record
 (define (inspect n)
   (let ([element (list-ref records n)])
@@ -40,11 +48,12 @@
 (read-record in pointer)
 (inspect 0)
 
-;turn decimal numbers into a string of bits for twiddling with
-(define (int->bits dec [top true])
-  (let([bin-buffer (list)])
-    (when (> dec 0)    
-      (set! bin-buffer (append bin-buffer (list (modulo dec 2))))
-      (set! bin-buffer (append bin-buffer (int->bits (floor (/ dec 2)) false))))
-    (when top (set! bin-buffer (reverse bin-buffer)))
-    bin-buffer))
+(define (bitlist->int bitlist)
+  (let ([value 0] [bits (reverse bitlist)])
+    (map (lambda (bit)
+           (set! value (* 2 value))
+           (set! value (+ bit value))) bits) value))
+
+
+;(bitlist->int (record-dollar-amount (list-ref records 0)))
+;10011100001111
