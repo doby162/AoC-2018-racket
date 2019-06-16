2227949f70078fed3df121ac48081ab6b07e313a
Michael Dorian <doby162@gmail.com>
2019-06-16 17:12:10 -0500

    debugging float

--- proto.rkt
+++ proto.rkt
@@ -45,7 +45,18 @@
   (list-tail (reverse (list-tail (reverse list)  (- (length list) distance beg)  )) beg))
 ;simulate a hardware float64
 (define (bitlist->float bitlist)
-  (let ([sign (list-ref bitlist 0)] [exp (bitlist->int (list-section bitlist 1 11))] [num (bitlist->int (list-tail bitlist 11))] )
+  (fprintf (current-output-port) "~n~nbyte1 ~a~nbyte2 ~a~nbyte3 ~a~nbyte4 ~a~nbyte5 ~a~nbyte6 ~a~nbyte7 ~a~nbyte8 ~a~n~n"
+           (list-section bitlist 0 8)
+           (list-section bitlist 8 8)
+           (list-section bitlist 16 8)
+           (list-section bitlist 24 8)
+           (list-section bitlist 32 8)
+           (list-section bitlist 40 8)
+           (list-section bitlist 48 8)
+           (list-section bitlist 52 8))
+  (print (length bitlist))
+  (print bitlist)
+  (let ([sign (list-ref bitlist 0)] [exp (bitlist->int (list-section bitlist 1 11))] [num (bitlist->int (list-tail bitlist 32))] );604
     (fprintf (current-output-port) "sign: ~a~nexp: ~a~nint: ~a~n" sign exp num)))
 
 
